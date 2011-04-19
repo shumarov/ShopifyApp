54bc41ba119f1b96a08b62f87a73be8c9872a99b
shumarov <shumarov@scand.com>
2011-04-19 16:54:07 +0300

    Test ShopifyApp

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,6 @@ class LoginController < ApplicationController
     # Ask user for their #{shop}.myshopify.com address
     
     # If the #{shop}.myshopify.com address is already provided in the URL, just skip to #authenticate
-    Rails.logger.info "asdasdasdasd"
     if params[:shop].present?
       redirect_to :controller => 'login', :action => "authenticate", :shop => params[:shop]
     end
